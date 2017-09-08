d7ceb55f5ab0fb465982435bbcb20933c4f2be83
Caio Azevedo <caioba19@gmail.com>
2017-09-08 14:33:38 -0300

    Create model for counselors

--- counselors/models.py
+++ counselors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Counselor(models.Model):
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=200, unique=True)
+    phone_number = models.CharField(max_length=14)
+    county = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=11)
