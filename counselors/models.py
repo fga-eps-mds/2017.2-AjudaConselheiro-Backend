5aebc12d7ac91eaf9869ac9695350de1bd683d62
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-09-08 18:40:42 -0300

    Create account model for Counselors

--- counselors/models.py
+++ counselors/models.py
@@ -1,9 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 
-class Counselor(models.Model):
-    full_name = models.CharField(max_length=250)
+class Counselor(AbstractBaseUser):
+    first_name = models.CharField(max_length=100, default='NOME')
+    last_name = models.CharField(max_length=100, default='SOBRENOME')
+    
     email = models.EmailField(max_length=200, unique=True)
-    phone_number = models.CharField(max_length=14)
+    phone = models.CharField(max_length=14)
     county = models.CharField(max_length=150)
     cpf = models.CharField(max_length=11)
+
+    tagline = str(first_name) + str(last_name)
+    full_name = str(first_name) + " " + str(last_name)
+
+    # criar classe CounselorManager 
+    # objects = CounselorManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def make_appointment(self):
+        pass
