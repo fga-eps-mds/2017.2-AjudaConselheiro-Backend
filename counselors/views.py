79d1a3bf4b76492e18ec3ee49407ea5add20cb86
Gabriel Ziegler <gabrielziegler3@gmail.com>
2017-09-11 01:24:15 -0300

    Create API permissions and viewset for Counselors

--- counselors/views.py
+++ counselors/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import permissions, viewsets, status
+from rest_framework.response import Response
 
-def index(request):
-    return HttpResponse("Página Conselheiro")
+from counselors.models import Counselor
+from counselors.permissions import IsCounselorOwner
+from counselors.serializers import CounselorSerializer
+
+class CounselorViewSet(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = Counselor.objects.all()
+    serializer_class = CounselorSerializer
+
+    def get_permissions(self):
+        if (self.request.method in permissions.SAFE_METHODS):
+            return (permissions.AllowAny(),)
+
+        if (self.request.method == 'POST'):
+            return (permissions.AllowAny(),)
+        
+        return (permissions.IsAuthenticated(), IsCounselorOwner(),)
+
+    def create(self, request):
+        serializer = self.serializer_class(data = request.data)
+
+        if (serializer.is_valid()):
+            Counselor.objects.create_user(**serializer.validated_data)
+            
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+
+        return Response(
+        {
+            'status': 'Bad request',
+            'message': 'Counselor could not be created with received data.'
+        }, status=status.HTTP_400_BAD_REQUEST)
